9b11ae6a10342425c2549de00746d8cc078f3109
Luna Chouchou <liu2shu1@outlook.com>
2024-07-28 22:07:26 +0900

    Image

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-    return ( 
-        <div>AboutPage</div>
-     );
+    return (
+        <div>
+            <div className={styles.imgContainer}>
+                <Image src="https://images.pexels.com/photos/18173391/pexels-photo-18173391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill/>
+            </div>
+        </div>
+    );
 }
- 
+
 export default AboutPage;
