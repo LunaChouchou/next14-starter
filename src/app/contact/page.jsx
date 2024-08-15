e6e458e61230ca48230a953ecfab04b464ee92fa
Luna Chouchou <liu2shu1@outlook.com>
2024-08-15 18:12:07 +0900

    Hydration Error Example

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,31 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./contact.module.css";
+import { useState, useEffect } from "react";
 
 const ContactPage = () => {
+  // Hydration Error
+  // const a = Math.random();
+  // console.log(a);
+
+  // Solution
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  const a = Math.random();
+  console.log(a, isClient);
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src="/contact.png" alt="" fill className={styles.img} />
       </div>
       <div className={styles.formContainer}>
+        {/* {a} */}
+        {isClient && a}
         <form action="" className={styles.form}>
           <input type="text" placeholder="Name and Surname"></input>
           <input type="text" placeholder="Email Adress"></input>
@@ -19,7 +37,7 @@ const ContactPage = () => {
             rows="10"
             placeholder="Message"
           ></textarea>
-          <button>Send</button>
+          <button onClick={() => {console.log("clicked")}}>Send</button>
         </form>
       </div>
     </div>
