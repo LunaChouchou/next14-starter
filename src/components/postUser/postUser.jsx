9fc33673b5dd2a389ee895c00a57bd90369da736
Luna Chouchou <liu2shu1@outlook.com>
2024-08-17 17:33:02 +0900

    Suspense

--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -1,7 +1,7 @@
 import styles from "./postUser.module.css";
 
 const getData = async (userId) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {cache: "no-store"});
 
   if (!res.ok) {
     throw new Error("Something went wrong");
@@ -15,7 +15,7 @@ const PostUser = async ({ userId }) => {
   return (
     <div className={styles.container}>
       <span className={styles.title}>Author</span>
-      <span className={styles.username}>{user.name}</span>
+      <span className={styles.username}>{user.username}</span>
     </div>
   );
 };
