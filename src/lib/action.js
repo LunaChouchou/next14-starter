d3801ed765e4a9976905e7c38cc6096d57e85a82
Luna Chouchou <liu2shu1@outlook.com>
2024-08-18 02:29:09 +0900

    Server Actions

---
+++ src/lib/action.js
@@ -0,0 +1,47 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { Post } from "./models";
+import { connectToDb } from "./utils";
+
+export const addPost = async (formData) => {
+  // "use server"
+
+  //   const title = formData.get("title");
+  //   const desc = formData.get("desc");
+  //   const slug = formData.get("slug");
+
+  const { title, desc, slug, userId } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+    const newPost = new Post({
+      title,
+      desc,
+      slug,
+      userId,
+    });
+
+    await newPost.save();
+    revalidatePath("/blog"); // fetch new data
+    console.log("saved to db");
+  } catch (err) {
+    console.log(err);
+    return { err: "Something went wrong!" };
+  }
+};
+
+export const deletePost = async (formData) => {
+  const { id } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+
+    await Post.findByIdAndDelete(id);
+    console.log("deleted from db");
+    revalidatePath("/blog");
+  } catch (err) {
+    console.log(err);
+    return { err: "Something went wrong!" };
+  }
+};
